e82e9fcda1263781c08926f3365e7aa2575c1deb
Kenneth Crawford <kmc059000@gmail.com>
2018-05-01 08:25:03 -0500

    removing unnecessary ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
+import { fetchTopStories } from './'
 
 class App extends React.Component {
+  public componentDidMount() {
+
+  }
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">hn clone</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit <code>src/App.tsx</code> and save to reload. Hello world
         </p>
       </div>
     );
