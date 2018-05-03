cfae2d80c11bcf87287fea7e7380154445cbcb09
Kenneth Crawford <kmc059000@gmail.com>
2018-05-03 08:42:32 -0500

    setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
 
 import StoryList from './components/StoryList';
@@ -9,12 +14,14 @@ class App extends React.Component<any, any> {
   }
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">hn clone</h1>
-        </header>
-        <StoryList />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">hn clone</h1>
+          </header>
+          <Route exact={true} path="/" component={StoryList} />
+        </div>
+      </Router>
     );
   }
 }
